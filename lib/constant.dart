cc560bc1c580bc252667697131541b9a1e041ac7
Léo Quelis <leo.quelis@etu.unice.fr>
2023-05-16 09:34:38 +0700

    ajout database

---
+++ lib/constant.dart
@@ -0,0 +1,3 @@
+const MONGO_URL =
+    "mongodb+srv://test:test@test.n9uqujc.mongodb.net/test?retryWrites=true&w=majority";
+const COLLECTION = "test";
---
+++ lib/database.dart
@@ -0,0 +1,30 @@
+import 'package:database/constant.dart';
+import 'package:mongo_dart/mongo_dart.dart';
+
+class Database {
+  late final Db db;
+
+  Database._(this.db);
+
+  static Future<Database> connect() async {
+    var db = await Db.create(MONGO_URL);
+    await db.open();
+    return Database._(db);
+  }
+
+  Future<List<Map<String, dynamic>>> getAllFromCollection(
+      String collectionName) async {
+    var collection = db.collection(collectionName);
+    return collection.find().toList();
+  }
+
+  Future<void> writeCollection(String collectionName, String params) async {
+    var collection = db.collection(collectionName);
+    if (collection == null) {
+      await db.createCollection(collectionName);
+    }
+    //Pas besoin d'id, celui-ci est généré automatiquement
+    await collection.insertOne({"name": params, "id": 10});
+    print(await collection.find().toList());
+  }
+}
