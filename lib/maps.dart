6a090e49cf2f3daa74d982dbd54c48f034d89510
thibo24 <thibault.ripoche@free.fr>
2023-05-29 04:25:36 +0200

    map qui marche avec les points

--- lib/maps.dart
+++ lib/maps.dart
@@ -1,11 +1,15 @@
+import 'package:donaso/database.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
-import 'database.dart';
+import 'package:location/location.dart';
+import 'dart:ui' as ui;
 
 class Maps extends StatefulWidget {
   String username;
   Database db;
+
   Maps({Key? key, required this.username, required this.db}) : super(key: key);
 
   @override
@@ -14,52 +18,107 @@ class Maps extends StatefulWidget {
 
 class MapsState extends State<Maps> {
   late GoogleMapController mapController;
-  final double maxDistance = 1000; // Distance maximale en mètres
+  final double maxDistance = 10000000000; // Distance maximale en mètres
   late Position position;
-  LatLng? userLocation;
-
   Set<Marker> markers = {};
+  Location location = Location();
+  BitmapDescriptor? basicBin;
+  BitmapDescriptor? glassBin;
+  BitmapDescriptor? Bin;
 
   @override
   void initState() {
     super.initState();
+    requestLocationPermission();
+    loadbasicBin();
     getCurrentLocation();
   }
 
-  Future<void> getCurrentLocation() async {
-    LocationPermission permission;
-    permission = await Geolocator.checkPermission();
+  Future<void> getMarkers() async {
+    var collection = await widget.db.findLocationsNearby(
+      position.longitude,
+      position.latitude,
+    );
+    setState(() {
+      markers.clear();
+      for (var location in collection) {
+        var coordinates = location['location']['coordinates'];
+        markers.add(
+          Marker(
+            markerId: MarkerId(location['_id'].toString()),
+            position: LatLng(coordinates[1], coordinates[0]),
+            icon: basicBin!,
+            infoWindow: InfoWindow(
+              title: location['description'],
+              snippet: location['description'],
+            ),
+          ),
+        );
+      }
+      print(markers.length);
+    });
+  }
+
+  Future<void> requestLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
       permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
-        return Future.error('Permissions denied');
+        return Future.error("non autorisé");
+        // L'utilisateur a refusé l'autorisation de localisation
       }
     }
-    position = await Geolocator.getCurrentPosition(
-      desiredAccuracy: LocationAccuracy.high,
+  }
+
+  Future<void> loadbasicBin() async {
+    try {
+      final Uint8List markerIconData = await getBytesFromAsset(
+        'assets/images/recyling-bin.png',
+        100,
+      );
+      setState(() {
+        basicBin = BitmapDescriptor.fromBytes(markerIconData);
+      });
+    } catch (e) {
+      print('Erreur lors du chargement de l\'icône du marqueur : $e');
+    }
+  }
+
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(
+      data.buffer.asUint8List(),
+      targetWidth: width,
     );
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
 
-    double latitude = position.latitude;
-    double longitude = position.longitude;
+  Future<void> getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Les services de localisation sont désactivés
+      // Demandez à l'utilisateur de les activer
+      return;
+    }
 
-    // Rechercher les positions à proximité
-    List<Map<String, dynamic>> nearbyLocations =
-        await widget.db.findLocationsNearby(latitude, longitude, maxDistance);
-
-    // Ajouter des marqueurs pour les positions à proximité
-    setState(() {
-      markers.clear();
-      for (var location in nearbyLocations) {
-        double lat = location['coordinates'][1];
-        double lng = location['coordinates'][0];
-        LatLng position = LatLng(lat, lng);
-
-        markers.add(Marker(
-          markerId: MarkerId(position.toString()),
-          position: position,
-        ));
+    LocationPermission permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // L'utilisateur a refusé l'autorisation de localisation
+        return;
       }
+    }
+
+    Position currentPosition = await Geolocator.getCurrentPosition();
+    setState(() {
+      position = currentPosition;
     });
+
+    getMarkers();
   }
 
   @override
@@ -74,6 +133,8 @@ class MapsState extends State<Maps> {
           zoom: 12.0,
         ),
         markers: markers,
+        myLocationEnabled: true,
+        myLocationButtonEnabled: false,
       ),
     );
   }
