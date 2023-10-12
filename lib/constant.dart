1f7da8931905f27dc1019bc7124f87af8d429a12
thibo24 <98901782+thibo24@users.noreply.github.com>
2023-10-12 16:54:32 +0200

    Update constant.dart

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,5 +1,5 @@
 const MONGO_URL =
-    "mongodb+srv://test:test@test.n9uqujc.mongodb.net/3TApp?retryWrites=true&w=majority?:tls=true";
+    "MangoUrl";
 const COLLECTION = "3TApp";
 const locName = "markers";
 const userTable = "users";
